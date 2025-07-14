ec50da867b28f53f72c513dae2e3aaa7eb62a3e5
skrodrigo <rodrigoa0987@gmail.com>
2025-07-13 22:03:29 -0300

    add pixel facebook

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Bricolage_Grotesque } from "next/font/google";
+import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 
 const _bricolageGrotesque = Bricolage_Grotesque({
@@ -29,6 +30,31 @@ export default function RootLayout({
 				>
 					{children}
 				</ThemeProvider>
+
+				{/* Meta Pixel Code */}
+				<Script id="meta-pixel-script" strategy="afterInteractive">
+					{`
+					!function(f,b,e,v,n,t,s)
+					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+					n.queue=[];t=b.createElement(e);t.async=!0;
+					t.src=v;s=b.getElementsByTagName(e)[0];
+					s.parentNode.insertBefore(t,s)}(window, document,'script',
+					'https://connect.facebook.net/en_US/fbevents.js');
+					fbq('init', '1879517062811289');
+					fbq('track', 'PageView');
+					`}
+				</Script>
+				<noscript>
+					<img
+						height="1"
+						width="1"
+						style={{ display: "none" }}
+						src="https://www.facebook.com/tr?id=1879517062811289&ev=PageView&noscript=1"
+					/>
+				</noscript>
+				{/* End Meta Pixel Code */}
 			</body>
 		</html>
 	);
