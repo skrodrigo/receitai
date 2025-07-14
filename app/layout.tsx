9f13d7b26e6a801fce198dafc75b3b7890e92654
skrodrigo <rodrigoa0987@gmail.com>
2025-07-14 08:00:12 -0300

    add script utmify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Bricolage_Grotesque } from "next/font/google";
-import Image from "next/image";
 import Script from "next/script";
 import { ThemeProvider } from "next-themes";
 import BackRedirect from "@/components/BackRedirect";
@@ -33,31 +32,18 @@ export default function RootLayout({
 					{children}
 				</ThemeProvider>
 
-				{/* Meta Pixel Code */}
-				<Script id="meta-pixel-script" strategy="afterInteractive">
-					{`
-					!function(f,b,e,v,n,t,s)
-					{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-					n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-					if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-					n.queue=[];t=b.createElement(e);t.async=!0;
-					t.src=v;s=b.getElementsByTagName(e)[0];
-					s.parentNode.insertBefore(t,s)}(window, document,'script',
-					'https://connect.facebook.net/en_US/fbevents.js');
-					fbq('init', '1879517062811289');
-					fbq('track', 'PageView');
-					`}
+				<Script id="utmify-pixel-config" strategy="afterInteractive">
+					{
+						`window.pixelId = "6874e2ed1fae0a5a8bf784b9";`
+					}
 				</Script>
-				<noscript>
-					<Image
-						height="1"
-						alt=""
-						width="1"
-						style={{ display: "none" }}
-						src="https://www.facebook.com/tr?id=1879517062811289&ev=PageView&noscript=1"
-					/>
-				</noscript>
-				{/* End Meta Pixel Code */}
+				<Script 
+					id="utmify-pixel-script-loader"
+					strategy="afterInteractive"
+					src="https://cdn.utmify.com.br/scripts/pixel/pixel.js"
+					async
+					defer
+				/>
 
 				<Script
 					id="utmify-script"
